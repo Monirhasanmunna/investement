9781a218545355ec905e8479c3b8ba0cf6f30535
MH Munna <monirhasanmunna1@gmail.com>
2025-08-30 21:58:08 +0600

    [Add] layout design issue solved

--- resources/js/Layouts/Client/Dashboard/Main.jsx
+++ resources/js/Layouts/Client/Dashboard/Main.jsx
@@ -9,13 +9,15 @@ export default function Main({children}){
         <>
             <Notifier/>
             <div className="max-w-xl min-h-[100vh] md:min-h-[600px] mx-auto bg-white flex flex-col md:border border-gray-400 md:shadow-2xl shadow-gray-200 md:rounded md:mt-10">
-                <header className="flex items-center justify-between px-4 py-3 border-b border-gray-300 bg-slate-800 text-gray-100 rounded-t md:rounded-t">
+                <header className="flex items-center justify-between px-4 py-3 border-b border-gray-300 bg-slate-800 text-gray-100 rounded-t md:rounded-t fixed top-0 left-0 right-0">
                     <h1 className="text-lg font-semibold">Hi! {auth.user.name}</h1>
                 </header>
 
-                {children}
+                <div className="w-ful mt-10">
+                    {children}
+                </div>
 
-                <footer className="mt-auto border-t border-gray-300 flex justify-around py-3 bg-slate-800">
+                <footer className="mt-auto border-t border-gray-300 flex justify-around py-3 bg-slate-800 fixed bottom-0 left-0 right-0">
                     <Link href={route('user.dashboard.dashboard')} className="text-gray-100 cursor-pointer">Home</Link>
                     <Link href={route('client.transection.list')} className="text-gray-100 cursor-pointer">Transections</Link>
                     <Link href={route('client.wallet.list')} className="text-gray-100 cursor-pointer">Wallet</Link>
